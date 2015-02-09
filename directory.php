65ff8514784a2609b149417133a92d148a8f6cea
Alex Bezuska <abezuska@gmail.com>
2015-02-09 15:27:08 -0500

    add contactable people

--- directory.php
+++ directory.php
@@ -4,35 +4,35 @@ $docId = "1W3S_j9vi58Y5BMTPLdWptTQwScMWv5KrkU-r9R2_dvU";
 //$url = "https://spreadsheets.google.com/feeds/list/" . $docId . "/od6/public/values?alt=json";
 $url = "http://spreadsheets.google.com/feeds/list/" . $docId . "/od6/public/values?alt=json&amp;callback=displayContent";
 
-$json = file_get_contents($url);
-$data = json_decode($json, TRUE);
+$json = file_get_contents( $url );
+$data = json_decode( $json, TRUE );
 
 $people = $data['feed']['entry'];
 
-function displayPeople($people, $badgeData){
+function displayPeople( $people, $badgeData ) {
 	$peopleHTML = "";
-	foreach ($people as $person) {
-		if($person['gsx$showonsite']['$t'] == "TRUE"){
-			$peopleHTML .= "<div class='person'>"; 
+	foreach ( $people as $person ) {
+		if ( $person['gsx$showonsite']['$t'] == "TRUE" ) {
+			$peopleHTML .= "<div class='person'>";
 			$peopleHTML .= "<div class='photoHolder'>";
-			$peopleHTML .= addPhoto($person);
- 			$peopleHTML .= addBadges($person, $badgeData);
+			$peopleHTML .= addPhoto( $person );
+			$peopleHTML .= addBadges( $person, $badgeData );
 			$peopleHTML .= "</div>";
 			$peopleHTML .= "<div class='personContent'>";
-			$peopleHTML .= addName($person);
- 			$peopleHTML .= addField($person, "Studio", "studio");
- 			$peopleHTML .= addField($person, "Bio", "bio");
- 			$peopleHTML .= addField($person, "Projects", "projects");
- 			$peopleHTML .= addField($person, "Skills", "skills");
- 			$peopleHTML .= addField($person, "Seeking", "seeking");
-			$peopleHTML .= addWebsite($person);
+			$peopleHTML .= addName( $person );
+			$peopleHTML .= addField( $person, "Studio", "studio" );
+			$peopleHTML .= addField( $person, "Bio", "bio" );
+			$peopleHTML .= addField( $person, "Projects", "projects" );
+			$peopleHTML .= addField( $person, "Skills", "skills" );
+			$peopleHTML .= addField( $person, "Seeking", "seeking" );
+			$peopleHTML .= addWebsite( $person );
 			$peopleHTML .= "<div class='social'>";
- 			$peopleHTML .= addEmail($person);
- 			$peopleHTML .= addTwitter($person);
- 			$peopleHTML .= addTumblr($person);
- 			$peopleHTML .= addGithub($person);
- 			$peopleHTML .= addGooglePlus($person);
-			$peopleHTML .= addSteam($person);
+			$peopleHTML .= addEmail( $person );
+			$peopleHTML .= addTwitter( $person );
+			$peopleHTML .= addTumblr( $person );
+			$peopleHTML .= addGithub( $person );
+			$peopleHTML .= addGooglePlus( $person );
+			$peopleHTML .= addSteam( $person );
 			$peopleHTML .= "</div>";
 			$peopleHTML .= "</div>";
 			$peopleHTML .= "</div>";
@@ -42,100 +42,100 @@ function displayPeople($people, $badgeData){
 	return $peopleHTML;
 }
 
-function countPeople($people){
+function countPeople( $people ) {
 	$count = 0;
-	foreach ($people as $person) {
-		if($person['gsx$showonsite']['$t'] == "TRUE"){
+	foreach ( $people as $person ) {
+		if ( $person['gsx$showonsite']['$t'] == "TRUE" ) {
 			$count ++;
 		}
 	}
 	return $count;
 }
 
-function addName($person){
+function addName( $person ) {
 	$name = "<h3>";
 	$firstname = $person['gsx$firstname']['$t'];
 	$lastname = $person['gsx$lastname']['$t'];
 	$nicknames = $person['gsx$nicknames']['$t'];
-	if(!empty($firstname)) {
+	if ( !empty( $firstname ) ) {
 		$name .= $firstname . " ";
 	}
-	if(!empty($nicknames)) {
+	if ( !empty( $nicknames ) ) {
 		$name .= "'" . $nicknames . "' ";
 	}
-	if(!empty($lastname)) {
+	if ( !empty( $lastname ) ) {
 		$name .= $lastname;
 	}
 	return $name . "</h3>";
 }
 
-function addPhoto($person) {
+function addPhoto( $person ) {
 	$photoURL = $person['gsx$photourl']['$t'];
-	if(empty($photoURL)){
+	if ( empty( $photoURL ) ) {
 		return "<img class='directoryPhoto' src='http://gamedevlou.org/wp-content/uploads/2015/02/nophoto.png'></img>";
 	}
 	return "<img class='directoryPhoto' src='" . $photoURL . "'></img>";
 }
 
-function addField($person, $name, $field) {
+function addField( $person, $name, $field ) {
 	$field = $person['gsx$' . $field]['$t'];
-	if(empty($field)){
+	if ( empty( $field ) ) {
 		return "";
 	}
 	return "<p><strong>" . $name . ": </strong>" . $field . "</a></p>";
 }
 
-function addEmail($person) {
+function addEmail( $person ) {
 	$email = $person['gsx$email']['$t'];
-	if(empty($email)){
+	if ( empty( $email ) ) {
 		return "";
 	}
 	return "<a href='mailto:" . $email . "'><i class='fa fa-envelope'></i></a>";
 }
 
-function addWebsite($person) {
+function addWebsite( $person ) {
 	$website = $person['gsx$website']['$t'];
-	if(empty($website)){
+	if ( empty( $website ) ) {
 		return "";
 	}
 	return "<p><strong>Website: </strong><a href='" . $website . "'>" . $website ."</a></p>";
 }
 
-function addTwitter($person) {
+function addTwitter( $person ) {
 	$twitterId = $person['gsx$twitter']['$t'];
-	if(empty($twitterId)){
+	if ( empty( $twitterId ) ) {
 		return "";
 	}
 	return "<a href='https://twitter.com/" . $twitterId . "'><i class='fa fa-twitter'></i></a>";
 }
 
-function addTumblr($person) {
+function addTumblr( $person ) {
 	$tumblrId = $person['gsx$tumblr']['$t'];
-	if(empty($tumblrId)){
+	if ( empty( $tumblrId ) ) {
 		return "";
 	}
 	return "<a href='http://" . $tumblrId . ".tumblr.com/'><i class='fa fa-tumblr'></i></a>";
 }
 
-function addGithub($person) {
+function addGithub( $person ) {
 	$githubId = $person['gsx$github']['$t'];
-	if(empty($githubId)){
+	if ( empty( $githubId ) ) {
 		return "";
 	}
 	return "<a href='http://github.com/" . $githubId . "'><i class='fa fa-github'></i></a>";
 }
 
-function addGooglePlus($person) {
+function addGooglePlus( $person ) {
 	$googleplusId = $person['gsx$googleplus']['$t'];
-	if(empty($googleplusId)){
+	if ( empty( $googleplusId ) ) {
 		return "";
 	}
 	return "<a href='http://googleplus.com/" .$googleplusId . "'><i class='fa fa-googleplus'></i></a>";
 }
 
-function addSteam($person) {
+function addSteam( $person ) {
 	$steamId = $person['gsx$steam']['$t'];
-	if(empty($steamId)){
+	if ( empty( $steamId ) ) {
 		return "";
 	}
 	return "<a href='http://steamcommunity.com/id/" . $steamId . "'><i class='fa fa-steam'></i></a>";
@@ -174,25 +174,37 @@ $badgeData = (object) array(
 	)
 );
 
-function addBadges($person, $data){
+function addBadges( $person, $data ) {
 	$badges = "<div class='badges'>";
 
-	foreach ($data as $badge) {
+	foreach ( $data as $badge ) {
 		$gameName = $person['gsx$' . $badge->name]['$t'];
 		$gameLink = $person['gsx$' . $badge->link]['$t'];
-		if(!empty($person['gsx$' . $badge->name]['$t'])){
+		if ( !empty( $person['gsx$' . $badge->name]['$t'] ) ) {
 			$badgeHTML = "<img class='badge' src='" . $badge->image . "' alt='" . $gameName . $badge->description . "' title='" . $gameName . $badge->description . "'/>";
-			if(!empty($gameLink)){
+			if ( !empty( $gameLink ) ) {
 				$badges .= "<a href='". $gameLink . "' target='_blank'>";
 				$badges .= $badgeHTML;
 				$badges .= "</a>";
-			}else{
+			}else {
 				$badges .= $badgeHTML;
 			}
 		}
 	};
 
 	return $badges .= "</div>";
+}
+
+function displayContactPeople( $people ) {
+	$contactPeople = "";
+	foreach ( $people as $person ) {
+		if ( $person['gsx$showonsite']['$t'] == "TRUE"
+			&& $person['gsx$cancontact']['$t'] == "TRUE"
+			&& !empty( $person['gsx$email']['$t'] ) ) {
+			$contactPeople .= "<a href='mailto:".$person['gsx$email']['$t']."' target='_blank'>".$person['gsx$firstname']['$t']."</a>, ";
+		}
+	}
+	return $contactPeople;
 }
 
 ?>
@@ -329,13 +341,15 @@ function addBadges($person, $data){
 	}
 	</style>
 
-	<h3> 
-		<span class="devCount"><?php echo countPeople($people); ?></span> 
-		Independant Game Developers!
-	</h3>
+<h3>
+	<span class="devCount"><?php echo countPeople( $people ); ?></span>
+	Independant Game Developers!
+</h3>
 
-	<div class="directoryPage">
-		<h3>Members:</h3>
-		<?php echo displayPeople($people, $badgeData); ?>
-	</div>
+<h1>New, or have questions?</h1>
+<p>Contact <?php echo displayContactPeople( $people ) ?> and we can help you out!</p>
 
+<div class="directoryPage">
+	<h3>Members:</h3>
+	<?php echo displayPeople( $people, $badgeData ); ?>
+</div>
