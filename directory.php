6603d96bef0b69fb1543e5e586168732beb5395f
Alex Bezuska <abezuska@gmail.com>
2015-02-09 13:11:00 -0500

    retrieve json!

--- directory.php
+++ directory.php
@@ -1,3 +1,12 @@
 <?php
- echo "hello world";
+
+
+$docId = "1W3S_j9vi58Y5BMTPLdWptTQwScMWv5KrkU-r9R2_dvU";
+$url = "https://spreadsheets.google.com/feeds/list/" . $docId . "/od6/public/values?alt=json";
+
+$json = file_get_contents($url);
+//echo $json;
+$json_data = json_decode($json);
+
+var_dump($json_data);
 ?>
